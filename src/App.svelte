ae9e0e4bda2d9791f521d750ddd2d275bdf3c0c8
TheBoringDude <iamcoderx@gmail.com>
2021-04-24 21:59:02 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
+<script lang="ts">
+  export let name: string;
+</script>
+
+<main class="flex items-center justify-center h-screen">
+  <div>
+    <h1 class="text-7xl mb-4">Hello {name}!</h1>
+    <p class:bg-red-100="{true}">
+      Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.
+    </p>
+  </div>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ["./src/**/*.svelte"],
+  mode: 'jit', // for faster editing
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    // if using a google font, . .
+    // fontFamily: {
+    //   sans: ['"Fira Sans"', 'sans-serif'],
+    // },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
